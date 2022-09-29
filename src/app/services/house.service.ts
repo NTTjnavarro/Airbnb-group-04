4dba165f7672be3f4f27800b192ec5e710bf8a3c
USERSAD\amoreiro <amoreiro@emeal.nttdata.com>
2022-09-29 19:15:37 +0200

    add list service

--- src/app/services/house.service.ts
+++ src/app/services/house.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { HouseDetail } from '../models/house-detail';
+import { GeoLocationModel } from '../models/geoLocation.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HouseService {
 
-  constructor(private httpClient: HttpClient) { }
-  getHouseById(id: number
-    ): Observable<HouseDetail> {
-      return this.httpClient.get<HouseDetail>('https://airbnb-learning-api.herokuapp.com/listings/'+id);
-    }
+  private url: string = 'https://airbnb-learning-api.herokuapp.com'
+
+  constructor(private httpClient: HttpClient) {}
+
+  getHouseById(id: number): Observable<HouseDetail> {
+    return this.httpClient.get<HouseDetail>(
+       this.url + '/listings/' + id
+    );
+  }
+
+  getHousesList(position: GeoLocationModel){
+    return this.httpClient.post(this.url, position)
+  }
+
 }
