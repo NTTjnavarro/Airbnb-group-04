1610f6950ff95f5452e3871e70c9773e481191e5
juanjose.navarroperea <jnavaper@emeal.nttdata.com>
2022-09-30 15:47:44 +0200

    create components

--- src/app/house-detail/house-detail.module.ts
+++ src/app/house-detail/house-detail.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { HouseDetailRoutingModule } from './house-detail-routing.module';
 import { HouseDetailComponent } from './components/house-detail/house-detail.component';
 import { CommonModule } from '@angular/common';
+import { ReserveComponent } from './components/reserve/reserve.component';
+import { EvaluationsComponent } from './components/evaluations/evaluations.component';
+import { MapComponent } from './components/map/map.component';
+import { HostComponent } from './components/host/host.component';
 
 
 @NgModule({
   declarations: [
-    HouseDetailComponent
+    HouseDetailComponent,
+    ReserveComponent,
+    EvaluationsComponent,
+    MapComponent,
+    HostComponent
   ],
   imports: [
     CommonModule,
