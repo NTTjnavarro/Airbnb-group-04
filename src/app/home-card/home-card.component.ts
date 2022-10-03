bea52168ba287aa0b9d0fa019a4f3cc1931c61a2
NTT.cabia@iberia.es <NTT.cabia@iberia.es>
2022-10-03 16:41:29 +0200

    no message

--- src/app/home-card/home-card.component.ts
+++ src/app/home-card/home-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HouseList } from '../models/house-detail';
+import { HouseService } from '../services/house.service';
 
 @Component({
   selector: 'app-home-card',
@@ -8,11 +9,16 @@ import { HouseList } from '../models/house-detail';
 })
 export class HomeCardComponent implements OnInit {
 
-  houseList: HouseList[]=[];
+  houseList: HouseList=[];
 
-  constructor() { }
+  constructor(private HouseService: HouseService) { 
+    this.HouseService.getHousesData().subscribe(houses => this.houseList=houses)
+  }
 
   ngOnInit(): void {
+    this.HouseService.getHousesData().subscribe(houses => this.houseList=houses)
+    console.log(this.houseList)
+
   }
 
 }
